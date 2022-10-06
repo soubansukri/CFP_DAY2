c38b00dbd630b3d5bd29b240aa14d76d2012ffd3
soubansukri <souban.sukri@gmail.com>
2022-10-06 17:36:31 +0530

    Added:Get Mapping through Request param method

--- Day_2_Assignment/src/main/java/com/bridgelabz/Day_2_Assignment/Controller/MessagingAppController.java
+++ Day_2_Assignment/src/main/java/com/bridgelabz/Day_2_Assignment/Controller/MessagingAppController.java
@@ -1,13 +1,21 @@
 package com.bridgelabz.Day_2_Assignment.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.Day_2_Assignment.UserDto.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MessagingAppController {
 
+    // UC-1=Welcome message
     @RequestMapping("/")
     public String welcomeMessage(){
         return "Hello from Bridgelabz";
     }
+
+    // UC-2=Get Request Param Method
+    @RequestMapping(value = "/Param",method = RequestMethod.GET)
+    public String ReqParamMessage(@RequestParam String name){
+        return "Hello" +" "+name+" from Bridgelabz";
+    }
+
 }
